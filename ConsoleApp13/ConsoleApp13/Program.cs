746dbce249b8ac9126c01e08abab2a17a09db339
Linus <73946659+Lian03001@users.noreply.github.com>
2021-02-17 15:14:34 +0100

    Add files via upload
    gjort till 6:00 i videon: "https://www.youtube.com/watch?v=bWPT5P2JJKk"

--- ConsoleApp13/ConsoleApp13/Program.cs
+++ ConsoleApp13/ConsoleApp13/Program.cs
@@ -3,54 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Diagnostics;
-using System.Threading;
 
-namespace ConsoleApp13
+namespace MergeSort
 {
     class Program
     {
-        static void Bubblesort(List<int> minLista)
-        {
-            for(int i = 0; i < minLista.Count; i++)
-            {
-                for(int j = 0; j < minLista.Count - 1 - i; j++)
-                {
-                    if (minLista[j] > minLista[j + 1])
-                    {
-                        int temp = minLista[j];
-                        minLista[j] = minLista[j + 1];
-                        minLista[j + 1] = temp;
-                    }
-                }
-            }
-        }
+        
         static void Main(string[] args)
         {
-            Stopwatch stopWatch = new Stopwatch();
+            var tallista = new int[16];
             var random = new Random();
-            var tallista = new List<int>();
-            for (int x = 0; x < 50000; x++)
+
+            for (int i = 0; i < tallista.Length; i++)
             {
-                tallista.Add(random.Next(50000));
+                tallista[i] = random.Next(10000);
             }
+            
+            Console.WriteLine("osorterad data");
+            DisplayArray(tallista);
 
-            stopWatch.Start();
-            Bubblesort(tallista);
-            stopWatch.Stop();
+            MergeSort(tallista);
 
-            foreach (int i in tallista)
-                Console.WriteLine(i);
+        }
 
-            // Get the elapsed time as a TimeSpan value.
-            TimeSpan ts = stopWatch.Elapsed;
+        private static void MergeSort(Span<int> tallista)
+        {
+            var center = tallista.Length / 2;
 
-            // Format and display the TimeSpan value.
-            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
-                ts.Hours, ts.Minutes, ts.Seconds,
-                ts.Milliseconds / 10);
-            Console.WriteLine("RunTime " + elapsedTime);
+            if (tallista.Length > 1)
+            {
+                MergeSort(tallista.Slice(0, center));
+                MergeSort(tallista.Slice(center));
+                MergeSort(tallista, center);
+            }
+        }
 
+        private static void DisplayArray(int[] tallista)
+        {
+            var text = string.Join(",", tallista);
+            Console.WriteLine(text);
         }
     }
 }
