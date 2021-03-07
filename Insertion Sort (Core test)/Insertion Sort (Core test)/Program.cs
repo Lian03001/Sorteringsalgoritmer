9a63be899e21b8f8c755213a8a563812e45e17dd
Linus <73946659+Lian03001@users.noreply.github.com>
2021-03-07 14:47:37 +0100

    Insertion Sort
    Med klocka, i .NET core

---
+++ Insertion Sort (Core test)/Insertion Sort (Core test)/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Insertion_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            Random random = new Random();
+            int[] tallista = new int[100000];
+            for (int i = 0; i < tallista.Length; i++)
+            {
+                tallista[i] = random.Next(1, 10000);
+            }
+
+            stopWatch.Start();
+            Insertionsort(tallista);
+            stopWatch.Stop();
+
+            foreach (var number in tallista)
+                Console.WriteLine(number);
+
+            // Get the elapsed time as a TimeSpan value.
+            TimeSpan ts = stopWatch.Elapsed;
+
+            // Format and display the TimeSpan value.
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
+            Console.WriteLine("hej");
+        }
+
+        private static void Insertionsort(int[] minlista)
+        {
+            for (int i = 0; i < minlista.Length - 1; i++)
+            {
+                for (int j = i + 1; j > 0; j--)
+                {
+                    if (minlista[j - 1] > minlista[j])
+                    {
+                        int temp = minlista[j - 1];
+                        minlista[j - 1] = minlista[j];
+                        minlista[j] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
