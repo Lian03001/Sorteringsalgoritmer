3e0d0d237ed359d3e9c04f595bc2d8ee78f9f92a
Linus <73946659+Lian03001@users.noreply.github.com>
2021-03-07 16:32:41 +0100

    Quick Sort
    med Stopwatch, i .NET Core

---
+++ Quick Sort/Quick Sort/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Quick_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            Random random = new Random();
+            int[] tallista = new int[100000];
+            for (int i = 0; i < tallista.Length; i++)
+            {
+                tallista[i] = random.Next(1, 10000);
+            }
+
+            stopWatch.Start();
+            SortArray(tallista);
+            stopWatch.Stop();
+
+            foreach (var number in tallista)
+                Console.WriteLine(number);
+
+            // Get the elapsed time as a TimeSpan value.
+            TimeSpan ts = stopWatch.Elapsed;
+
+            // Format and display the TimeSpan value.
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
+            Console.WriteLine("hej");
+        }
+
+        private static void SortArray(int[] numbers)
+        {
+            Quicksort(numbers, 0, numbers.Length - 1);
+        }
+
+        private static void Quicksort(int[] numbers, int left, int right)
+        {
+            int i = left;
+            int j = right;
+
+            var pivot = numbers[(left + right) / 2];
+
+            while (i <= j)
+            {
+                while (numbers[i] < pivot)
+                    i++;
+
+                while (numbers[j] > pivot)
+                    j--;
+
+                if (i <= j)
+                {
+                    var tmp = numbers[i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = tmp;
+
+                    i++;
+                    j--;
+                }
+            }
+
+            if (left < j)
+                Quicksort(numbers, left, j);
+
+            if (i < right)
+                Quicksort(numbers, i, right);
+        }
+    }
+}
